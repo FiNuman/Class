d06470b1d3ee3b12317ebafde3231c09e9c86d5e
Abdullah Al Numan <42403401+FiNuman@users.noreply.github.com>
2021-08-29 16:44:28 +0600

    Add files via upload

---
+++ Class13/jog.js
@@ -0,0 +1,8 @@
+function additionn(){
+  var a = document.getElementById("a1").value;
+  var b = document.getElementById("a2").value;
+  n1 =parseInt(a);
+  n2 =parseInt(b);
+  var c=n1+n2;
+  document.getElementById('a3').innerHTML=c;
+}
